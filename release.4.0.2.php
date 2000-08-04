2d9148dd93b21768e3f99221af5a91d031da4d1b
Zak Greant <zak@php.net>
2000-08-04 15:36:54 +0000

    Removed pre tags - converted to br tags, etc... Thats what I get for being lazy :)

--- release.4.0.2.php
+++ release.4.0.2.php
@@ -6,82 +6,82 @@ Open PHP 4 Bugs (PHP/QAT: The PHP Quality Assurance Team)
 </title>
 </head>
 <body bgcolor="#FFFFFF" background="img/bg_page.gif" text="#000000" link="#0000FF"
-	vlink="#009999" marginheight="0" marginwidth="0"
-	topmargin="0" leftmargin="0">
+    vlink="#009999" marginheight="0" marginwidth="0"
+    topmargin="0" leftmargin="0">
 
 <table cellpadding="0" cellspacing="0" border="0" width="619">
 <tr>
-	<td colspan="2" background="img/bg_green.gif"><img SRC="img/title.gif" WIDTH="202" HEIGHT="41" border="0" alt="PHP/QAT: The PHP Quality Assurance Team"></td>
+    <td colspan="2" background="img/bg_green.gif"><img SRC="img/title.gif" WIDTH="202" HEIGHT="41" border="0" alt="PHP/QAT: The PHP Quality Assurance Team"></td>
 </tr>
 <tr>
-	<td valign="top">
-		<font face="Tahoma, Arial, Helvetica, Sans Serif" size="3">
-		<div style="text-align: right; padding: 5">
-		<font size="1" face="MS SANS SERIF, GENEVA, SANS SERIF">
-		&gt; &gt; &gt;
-		<a href="index.php">Home</a> |
-		<a href="index.php#news">News</a> |
-		<a href="goals.php">Projects and Goals</a> |
-		<a href="resources.php">Links &amp; Resources</a> |
-		<a href="members.php">Members</a> |
-		<a href="howtohelp.php">How to help</a>
-		</font>
-		</div>
+    <td valign="top">
+        <font face="Tahoma, Arial, Helvetica, Sans Serif" size="3">
+        <div style="text-align: right; padding: 5">
+        <font size="1" face="MS SANS SERIF, GENEVA, SANS SERIF">
+        &gt; &gt; &gt;
+        <a href="index.php">Home</a> |
+        <a href="index.php#news">News</a> |
+        <a href="goals.php">Projects and Goals</a> |
+        <a href="resources.php">Links &amp; Resources</a> |
+        <a href="members.php">Members</a> |
+        <a href="howtohelp.php">How to help</a>
+        </font>
+        </div>
 <blockquote>
-<pre><font size="3">
-Testers for upcoming PHP 4.0.2 release
+<font size="3">
+Test Pool for upcoming PHP 4.0.2 release<br /><br />
 
-<b>Cameron Brunner</b> <a href="mailto:gamr@gattcomp.com.au">gamr@gattcomp.com.au</a>
-    Build and Test
-     - FreeBSD 4.0-RELEASE / Apache 1.3.12 / MySQL 3.22.32 with GD, IMAP,
-       and all sorts of other goodies
+<b>Cameron Brunner</b> <a href="mailto:gamr@gattcomp.com.au">gamr@gattcomp.com.au</a><br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - FreeBSD 4.0-RELEASE / Apache 1.3.12 / MySQL 3.22.32 with GD, IMAP,
+       and all sorts of other goodies<br /><br />
 
-<b>Derick Rethans</b> <a href="mailto:d.rethans@jdimedia.nl">d.rethans@jdimedia.nl</a>
-    Build and Test
-     - Linux (RH 6.2) / Apache 1.3.12 / PHP ( :-) / GD with PNG/JPEG/TTF/
-     / MySQL / mcrypt 2.[24].x / libpdf 3 / imap.
-     - win98 / pws - possibly?
+<b>Derick Rethans</b> <a href="mailto:d.rethans@jdimedia.nl">d.rethans@jdimedia.nl</a><br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Linux (RH 6.2) / Apache 1.3.12 / PHP ( :-) / GD with PNG/JPEG/TTF/
+     / MySQL / mcrypt 2.[24].x / libpdf 3 / imap<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - win98 / pws - possibly?<br /><br />
 
-<b>James Moore</b> (<a href="mailto:james@phpuk.org">james@phpuk.org</a>)
-    Build and Test
-     - Redhat 6.2
-     - Mandrake 7
-     - Windows 95,98,2000
+<b>James Moore</b> (<a href="mailto:james@phpuk.org">james@phpuk.org</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Redhat 6.2<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Mandrake 7<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Windows 95,98,2000<br /><br />
 
-<b>Joey Smith</b> (<a href="mailto:joey@php.net">joey@php.net</a>)
-    Build and Test
-     - Debian Linux (2.1, 2.2) - CGI/Apache Module, Sybase, Mysql,
+<b>Joey Smith</b> (<a href="mailto:joey@php.net">joey@php.net</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Debian Linux (2.1, 2.2) - CGI/Apache Module, Sybase, Mysql,
        PDFLib, GD, DOM/XML/expat, IMAP<br />
-     - Solaris 2.6 - modules as platform 1<br /><br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Solaris 2.6 - modules as platform 1<br /><br />
 
-<b>Kirill Maximov</b> (<a href="mailto:maxkir@email.com">maxkir@email.com</a>)
-	Build and Test
-	 - Solaris 7 x86, Oracle7.3.2, Mysql 3.22.32, sysV semaphores,
-	   GD 1.8.3/FreeType
+<b>Kirill Maximov</b> (<a href="mailto:maxkir@email.com">maxkir@email.com</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Solaris 7 x86, Oracle7.3.2, Mysql 3.22.32, sysV semaphores,
+       GD 1.8.3/FreeType<br /><br />
 
-<b>Patrik Bengtsson</b> (<a href="mailto:patrik.bengtsson@grafix.se">patrik.bengtsson@grafix.se</a>)
-	Build and Test
-	 - MacOSX Server 1.0 / Apache 1.3.12
-	 - SuSe Linux 6.4 / Apache 1.3.12
+<b>Patrik Bengtsson</b> (<a href="mailto:patrik.bengtsson@grafix.se">patrik.bengtsson@grafix.se</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - MacOSX Server 1.0 / Apache 1.3.12<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - SuSe Linux 6.4 / Apache 1.3.12<br /><br />
 
-<b>Phil Driscoll</b> (<a href="mailto:phil@dialsolutions.co.uk">phil@dialsolutions.co.uk</a>)
-	Build and Test
-	 - Win NT4 SP6 IIS4 cgi MySQL
+<b>Phil Driscoll</b> (<a href="mailto:phil@dialsolutions.co.uk">phil@dialsolutions.co.uk</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Win NT4 SP6 IIS4 cgi MySQL<br /><br />
 
-<b>waldschrott</b> (<a href="mailto:waldschrott@waldschrott.de">waldschrott@waldschrott.de</a>)
-    Build and Test
-     - W2k / Apache 1.3.12 /CGI+Mod, I´ll try ODBC,mysql,GD
+<b>waldschrott</b> (<a href="mailto:waldschrott@waldschrott.de">waldschrott@waldschrott.de</a>)<br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - W2k / Apache 1.3.12 /CGI+Mod, I´ll try ODBC,mysql,GD<br /><br />
 
-<b>Zak</b> (<a href="mailto:zak@php.net">zak@php.net</a>)<br />
-    Build and Test
-     - OpenBSD 2.7 / Apache 1.3.12
-     - BSDi 4.0 / Apache 1.3.12
+<b>Zak</b> (<a href="mailto:zak@php.net">zak@php.net</a>)<br /><br />
+&nbsp;&nbsp;&nbsp;&nbsp; Build and Test<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - OpenBSD 2.7 / Apache 1.3.12<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - BSDi 4.0 / Apache 1.3.12<br /><br />
 
-    Test, no build
-     - Win98 / Apache 1.3.12 / MySQL 3.23.31 beta
-     - Win2k Pro / IIS / OmniHTTPd / Xitami / Apache 1.3.12
-       / MySQL 3.23.31 beta
-</font></pre>
+&nbsp;&nbsp;&nbsp;&nbsp;Test, no build<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Win98 / Apache 1.3.12 / MySQL 3.23.31 beta<br />
+&nbsp;&nbsp;&nbsp;&nbsp; - Win2k Pro / IIS / OmniHTTPd / Xitami / Apache 1.3.12
+       / MySQL 3.23.31 beta<br /><br />
+</font>
 <? include ('inc/footer.txt') ?>
 
 <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font face="ms sans serif, arial, geneva, sans serif" size="3">$Author$ / $Date$ / $Revision$</font><br /><br />
