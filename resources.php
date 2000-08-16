7447e9b221f0d5b7fe964a3daca3c9fa9766b732
Zak Greant <zak@php.net>
2000-08-16 00:41:46 +0000

    Added and updated links

--- resources.php
+++ resources.php
@@ -34,16 +34,40 @@ Links and Resources (PHP/QAT: The PHP Quality Assurance Team)
 		<hr size="3" />
 		Please suggest links and resources for this page!  Email the PHP/QA list (<a href="mailto:php-qa@lists.php.net">php-qa@lists.php.net</a>)<br /><br />
 
-		&nbsp;* <a href="http://www.php.net/version4/cvs.php">Instructions for retrieving the latest PHP source via CVS</a><br /><br />
-		&nbsp;* <a href="http://www.php4win.de/">[napp] (not another php portal) Basic support for PHP on win32. The main focus is providing binary builds of PHP.</a><br /><br />
+		<ul>
+		<br /><br /><b>Official PHP Resources</b>
+		<li><a href="http://bugs.php.net/" add_date="966377539" last_visit="966377539" last_modified="966377686">PHP: Bug Reporting</a>
+		<li><a href="http://cvs.php.net/" add_date="966377525" last_visit="966377525" last_modified="960652638">PHP: CVS Repository</a>
+		<li><a href="http://www.php.net/docs.php" add_date="966377419" last_visit="966377419" last_modified="963652549">PHP: Documentation</a>
+		<li><a href="http://www.php.net/FAQ.php" add_date="966377361" last_visit="966377361" last_modified="965743021">PHP: Frequently Asked Questions</a>
+		<li><a href="http://www.php.net/links.php" add_date="966377735" last_visit="966377735" last_modified="964239569">PHP: Links</a>
+		<li><a href="http://www.zend.com/" add_date="953877076" last_visit="953877076" last_modified="964202078">Zend / Where PHP meets eBusiness</a>
 
-		&nbsp;* <a href="http://www.php.net/version4/win32build.php">PHP 4.0 build instructions - Win32 platform</a><br /><br />
+		<br /><br /><b>Build &amp; Install Instructions</b>
+		<li><a href="http://www.php.net/version4/cvs.php" add_date="966377830" last_visit="966377830" last_modified="957397465">Building the latest PHP source from CVS</a>
+		<li><a href="http://www.geocities.com/ro_marius/mod_php4.html">mod_php4 - How To</a>
+		<li><a href="http://www.php.net/version4/win32build.php" add_date="966377863" last_visit="966377863" last_modified="954170759">PHP Build Instructions for Win32</a>
 
-		&nbsp;* <a href="http://www.geocities.com/ro_marius/mod_php4.html">PHP4 module for Apache Win32 : HOW TO</a><br /><br />
+		<br /><br /><b>Build &amp; Install Tools</b>
+		<li><a href="http://www.gnu.org/software/autoconf/autoconf.html">Autoconf</a></li>
+		<li><a href="http://www.gnu.org/software/automake/automake.html">Automake</a></li>
+		<li><a href="http://www.gnu.org/software/bison/bison.html">Bison</a></li>
+		<li><a href="http://www.cvshome.org/">CVS</a></li>
+		<li><a href="http://www.gnu.org/software/flex/flex.html">Flex</a></li>
+		<li><a href="http://www.gnu.org/software/gcc/gcc.html">GCC: The GNU Compiler Collection</a></li>
+		<li><a href="http://www.gnu.org/software/gdb/gdb.html">gdb: GNU Debugger</a></li>
+		<li><a href="http://www.gnu.org/software/gzip/gzip.html">gzip</a></li>
+		<li><a href="http://www.gnu.org/software/libtool/libtool.html">libtool</a></li>
+		<li><a href="http://www.gnu.org/software/m4/m4.html">m4</a></li>
+		<li><a href="http://www.gnu.org/software/make/make.html">Make</a></li>
+		<li><a href="http://www.gnu.org/software/tar/tar.html">tar</a></li>
+		<li><a href="http://www.perl.com/">Perl</a></li>
 
-		&nbsp;* <a href="http://www.php.net/">The PHP Website</a><br /><br />
+		<br /><br /><b>Other PHP Resources</b>
+		<li><a href="http://www.zugeschaut-und-mitgebaut.de/php/" add_date="966373439" last_visit="966378803" last_modified="966346338">Hartmut Holzgraefe's PHP Function & Documentation Resource</a></li>
+		<li><a href="http://www.php4win.de/">[napp] (not another php portal) Basic support for PHP on win32. The main focus is providing binary builds of PHP.</a></li>
+		</ul>
 
-		&nbsp;* <a href="http://www.zend.com/">The Zend Website</a><br /><br />
 
 		<br /><br />
 
