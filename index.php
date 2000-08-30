0e798d1d9d28c2655dcaf48b61e0b49f014ec3ae
Zak Greant <zak@php.net>
2000-08-30 18:49:20 +0000

    Changed index page over to new code (kindly provided by Lars Torben Wilson)

--- index.php
+++ index.php
@@ -1,78 +1,63 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-<title>
-PHP/QAT: The PHP Quality Assurance Team
-</title>
-</head>
-<body bgcolor="#FFFFFF" background="img/bg_page.gif" text="#000000" link="#0000FF"
-	vlink="#009999" marginheight="0" marginwidth="0"
-	topmargin="0" leftmargin="0">
+<?php
+	$TITLE = 'PHP|QAT: The PHP Quality Assurance Team';
+	$CVS_KEYWORDS = '$Author$ / $Date$ / $Revision$';
 
-<table cellpadding="0" cellspacing="0" border="0" width="619">
-<tr>
-	<td colspan="2" background="img/bg_green.gif"><img SRC="img/title.gif" WIDTH="202" HEIGHT="41" border="0" alt="PHP/QAT: The PHP Quality Assurance Team"></td>
-</tr>
-<tr>
-	<td valign="top">
-		<font face="Tahoma, Arial, Helvetica, Sans Serif" size="3">
-		<div style="text-align: right; padding: 5">
-		<font size="3" face="MS SANS SERIF, GENEVA, SANS SERIF">
-		&gt; &gt; &gt;
-		<a href="index.php#news">News</a> |
-		<a href="goals.php">Projects and Goals</a> |
-		<a href="resources.php">Links &amp; Resources</a> |
-		<a href="members.php">Members</a> |
-		<a href="howtohelp.php">How to help</a>
-		</font>
-		</div>
-		<blockquote>
+	include ('../inc/header.inc');
+?>
+<h1>Welcome to the PHP Quality Assurance Team Web Page.</h1>
+<p id="blueback">
+	The PHP/QA Team is a small group of developers whose
+	<b>primary goal</b> is to support the PHP core developers
+	by providing them with timely quality assurance.
+</p>
+<p>
+	The team is currently focused on several areas:
+</p>
+<ul type="disc">
+	<li><span class="lihack">Testing the 4.0.2 Release Candidate</span></li>
+	<li><span class="lihack">Developing a test suite for PHP releases</span></li>
+	<li><span class="lihack">Reworking areas of the bug reporting process.</span></li>
+	<li><span class="lihack">Processing the open bug reports from the official PHP
+		4 bug list (<a href="http://bugs.php.net">http://bugs.php.net</a>).</span></li>
+</ul>
+<p>
+	If you would like to contribute to this effort, please
+	visit our <a href="howtohelp.php">How To Help</a> page.
+</p>
 
-		<br />
-		<b>Welcome to the PHP Quality Assurance Team Web Page.</b>
-		<hr size="3" />
-		<div style="background: #DDEEFF; padding: 5px">
-		The PHP/QA Team is a small group of developers whose <b>primary goal</b> is to support the PHP core developers by providing them with timely quality assurance.
-		</div>
-		<br />
-		The team is currently focused on several areas:<br />
-        <li>Testing the 4.0.2 Release Candidate<br />
-        <li>Developing a test suite for PHP releases<br />
-        <li>Reworking areas of the bug reporting process.<br />
-        <li>Processing the open bug reports from the official PHP 4 bug list ( <a href="http://bugs.php.net">http://bugs.php.net</a> ).<br /><br />
+<h2><a name="news">News</a></h2>
+<ul>
+	<li><span class="lihack">(August 24, 2000) PHP 4.0.2 scheduled for release next Monday (August 28th)</span></li>
+	<li><span class="lihack">(August 20, 2000) Two new roughs for site available at <a href="http://qa.php.net/site_rough">http://qa.php.netsite_rough</a></span></li>
+	<li><span class="lihack">(August 20, 2000) Modified Win32 Binaries available at <a href="http://www.php4win.de/">http://www.php4win.de</a></span></li>
+	<li><span class="lihack">(August 11, 2000) 4.0.2 RC now available via CVS.	Win32 binaries should be ready soon.</span></li>
+</ul>
 
-		If you would like to contribute to this effort, please visit our <a href="howtohelp.php">How To Help</a> page.
-		<br /><br /><br />
+<h2>Information</h2>
+<h3>Projects &amp; Goals</h3>
+<p>
+	(<a href="goals.php">View Projects &amp; Goals</a>)<br />
+	Find more information about PHP/QA Team's current projects
+	and future goals.
+</p>
 
-		<b><a name="news">News</a></b>
-		<hr size="3" />
-		<font size="3">
-		<li>( August 29, 2000 ) Barring any last minute problems, PHP 4.0.2 will be released today</li>
-		<li>( August 24, 2000 ) PHP 4.0.2 scheduled for release Monday, August 28</li>
-		</font>
-		<br /><br /><br />
+<h3>Links &amp; Resources</h3>
+<p>
+	(<a href="resources.php">View Links &amp; Resources</a>)<br />
+	Visit the PHP/QAT Links &amp; Resources page.	We have
+	links to web based resources for ...
+</p>
 
-		<b>PHP/QAT Information</b>
-		<hr size="3" />
-		<b>Projects & Goals</b> <font size="3" face="MS SANS SERIF, GENEVA, SANS SERIF">( <a href="goals.php">View Projects & Goals</a> )</font>
-		<br />
-		Find more information about PHP/QA Team's current projects and future goals.<br /><br />
+<h3>Members</h3>
+<p>
+	(<a href="members.php">View Members</a>)<br />
+	View the PHP/QAT Member register.
+</p>
 
-		<b>Links &amp; Resources</b> <font size="3" face="MS SANS SERIF, GENEVA, SANS SERIF">( <a href="resources.php">View Links &amp; Resources</a> )</font>
-		<br />
-		Visit the PHP/QAT Links &amp; Resources page.  We have links to web based resources for ...<br /><br />
-
-		<b>Members</b> <font size="3" face="MS SANS SERIF, GENEVA, SANS SERIF">( <a href="members.php">View Members</a> )</font>
-		<br />
-		View the PHP/QAT Member register.<br /><br />
-
-		<b>How To Help</b> <font size="3" face="MS SANS SERIF, GENEVA, SANS SERIF">( <a href="howtohelp.php">View How to Help</a> )</font>
-		<br />
-		Find out how you can help us - join the PHP/QA team, help out on a project or just make a suggestion.
-		<br /><br />
-
-		<? include ('inc/footer.txt') ?>
-
-<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font face="ms sans serif, arial, geneva, sans serif" size="3">$Author$ / $Date$ / $Revision$</font><br /><br />
-</body>
-</html>
+<h3>How To Help</h3>
+<p>
+	(<a href="howtohelp.php">View How to Help</a>)<br />
+	Find out how you can help us - join the PHP/QA team, help
+	out on a project or just make a suggestion.
+</p>
+<?php include ('../inc/footer.inc'); ?>
