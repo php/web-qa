9230f9396d90dd23c719de56e43f77721468cc65
Zak Greant <zak@php.net>
2000-10-05 02:34:00 +0000

    Hacked together a phenomenally ugly page (harvest.php) that gathers additional data based on the how the filter.php form is filled out. Amazing bad code :)

--- build_tracker/filter.php
+++ build_tracker/filter.php
@@ -1,126 +1,4 @@
-<?php
-
-# file2select
-# Author: j.a.greant 2000/08/23 zak@php.net
-
-# A simple function for generating select boxes from formatted text files
-# format for text files is:  option value::option display name
-# each entry should be separated by a newline
-# blank lines and lines that start with a single # are ignored
-
-function file2select ($field_name)
-{
-	$field[$field_name] = $GLOBALS[field][$field_name];
-	$selected[$field[$field_name]] = ' selected';
-
-	foreach (file ($field_name.'_list.txt') as $temp)
-	{
-		$temp = trim ($temp);
-		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
-
-		list ($value, $option) = explode ('::', $temp);
-		$option
-			or $option = $value;
-		$output .= sprintf ('<option value="%s"%s>%s</option>'."\n", $value, $selected[$value], $option);
-	}
-
-	return '<select name="field[' . $field_name . ']">' . "\n$output" .	'</select>';
-}
-
-# file2checkbox
-# Author: j.a.greant 2000/10/03 zak@php.net
-
-# A simple function for generating one or more checkboxes from formatted text files
-# format for text files is:  option value::option display name
-# each entry should be separated by a newline
-# blank lines and lines that start with a single # are ignored
-
-function file2checkbox ($field_name)
-{
-	$field[$field_name] = $GLOBALS[field][$field_name];
-	if(is_array ($field[$field_name]))
-		foreach ($field[$field_name] as $item)
-			$checked[$item] = ' checked';
-
-	foreach (file ($field_name.'_list.txt') as $temp)
-	{
-		$temp = trim ($temp);
-		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
-
-		list ($value, $option) = explode ('::', $temp);
-		$option
-			or $option = $value;
-		$output .= sprintf ('<input type="checkbox" name="field[%s]" value="%s"%s /> %s<br />'."\n", $field_name, $value, $selected[$value], $option);
-	}
-
-	return "\n$output";
-}
-
-function file2cb_column ($field_name)
-{
-	$field[$field_name] = $GLOBALS[field][$field_name];
-
-	if(is_array ($field[$field_name]))
-		foreach ($field[$field_name] as $item)
-			$checked[$item] = ' checked';
-
-	$modules = file ($field_name.'_list.txt');
-	$no_of_cells = count ($modules);
-	$row_length  = ceil ($no_of_cells/3);
-
-
-
-	for ($n=0, $col = 0, $row = 0; $n < $no_of_cells; ++$n)
-	{
-		$temp = trim ($modules[$n])
-			or $temp = '&nbsp';
-
-		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
-
-		list ($value, $option) = explode ('::', $temp);
-
-		$option
-			or $option = $value;
-
-		$line  = '<td><font face="Tahoma, Arial, Helvetica, sans-serif" SIZE="2">';
-		$line .= sprintf ('<input type="checkbox" name="field[%s][]" value="%s" %s /> %s ', $field_name, $value, $checked[$value], $option);
-		$line .= '</font></td>';
-
-		$cell[$col][$row] = $line;
-
-		++$row;
-		if ($row == $row_length)
-		{
-			$row = 0;
-			++$col;
-		}
-	}
-
-	for ($row = 0; $cell[0][$row]; ++$row)
-		$out .= '<tr bgcolor="#F0F0F0">'.$cell[0][$row].$cell[1][$row].$cell[2][$row].'</tr>';
-
-	return '<table border="1" width="100%">'.$out.'</table>';
-}
-
-# generate_row
-# Author: j.a.greant 2000/08/23 zak@php.net
-
-# A simple function for generating formatted rows with a varying # of columns
-
-function generate_row ($column_data, $no_of_columns = 2)
-{
-	$row = '<tr bgcolor="#F0F0F0">';
-
-	for ($index=0; $index < $no_of_columns; ++$index)
-	{
-		$row .= '<td><font face="Tahoma, Arial, Helvetica, sans-serif">';
-		$row .= $column_data[$index];
-		$row .= '</font></td>';
-	}
-
-	return $row . '</tr>';
-}
-?>
+<?php include 'functions.php'; ?>
 <html>
 <head>
 <title>
@@ -129,7 +7,7 @@ PHP|QAT: PHP Build and Install Tracker
 </head>
 <body bgcolor="#FFFFFF" text="#000000" link="#990000" vlink="#999900">
 <font face="Tahoma, Arial, Helvetica, Sans Serif">
-<form action="<?= $PHP_SELF ?>" method="POST">
+<form action="harvest.php" method="POST">
 <center>
 <h3>PHP Build and Install Tracker</h3>
 
@@ -174,8 +52,7 @@ PHP|QAT: PHP Build and Install Tracker
 			<?= generate_row (array ('&nbsp;<b>3.2 (b)</b> What version of the above operating system are you using?', '<input type="text" name="field[os_version]" value="'.htmlentities (stripslashes ($field[os_version])).'" size="32" maxlength="56" />')) ?>
 			<tr bgcolor="#F0F0F0"><td colspan="2"><font face="Tahoma, Arial, Helvetica, sans-serif">&nbsp;<b>3.3</b> What web server(s) do you use with this build of PHP?</font></td></tr>
 			<tr bgcolor="#F0F0F0"><td colspan="2"><?= file2cb_column ('server') ?></td></tr>
-			<?= generate_row (array ('&nbsp;<b>3.4 (a)</b> If you compiled PHP, please enter the configure line here:', '<textarea name="field[configure]" rows="2" cols="30">'.$field[configure].'</textarea>')) ?>
-			<?= generate_row (array ('&nbsp;<b>3.4 (b)</b> If you installed a binary, please enter the output of phpinfo() here:', '<textarea name="field[configure]" rows="2" cols="30">'.$field[phpinfo].'</textarea>')) ?>
+			<?= generate_row (array ('&nbsp;<b>3.4</b> If you compiled PHP, please enter the configure line here:', '<textarea name="field[configure]" rows="2" cols="30" wrap="virtual">'.$field[configure].'</textarea>')) ?>
 		</table>
 
 		</td>
--- build_tracker/functions.php
+++ build_tracker/functions.php
@@ -1,5 +1,18 @@
 <?php
 
+function widget ($group_name, $field_name, $note_desc = '&nbsp;', $long_note = 0)
+{
+	$field[$group_name][$field_name] = $GLOBALS[field][$group_name][$field_name];
+	$var_name = "field[$field_name][$name]";
+	$out[] = '<input type="text" name="'.$var_name[version].'" value="'.htmlentities (${$var_name.'[version]'}).'" />';
+	$out[] = '&nbsp;&nbsp;&nbsp;&nbsp;'.$note_desc;
+	if ($long_note)
+		$out[] = '<textarea name="'.$var_name[notes].'" rows="2" cols="30" wrap="virtual">'.htmlentities (${$var_name.'[notes]'}).'</textarea>';
+	else
+		$out[] = '<input type="text" name="'.$var_name[notes].'" value="'.htmlentities (${$var_name.'[notes]'}).'" />';
+	return $out;
+}
+
 # file2select
 # Author: j.a.greant 2000/08/23 zak@php.net
 
@@ -18,12 +31,88 @@ function file2select ($field_name)
 		$temp = trim ($temp);
 		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
 
-
 		list ($value, $option) = explode ('::', $temp);
+		$option
+			or $option = $value;
 		$output .= sprintf ('<option value="%s"%s>%s</option>'."\n", $value, $selected[$value], $option);
 	}
 
 	return '<select name="field[' . $field_name . ']">' . "\n$output" .	'</select>';
+}
+
+# file2checkbox
+# Author: j.a.greant 2000/10/03 zak@php.net
+
+# A simple function for generating one or more checkboxes from formatted text files
+# format for text files is:  option value::option display name
+# each entry should be separated by a newline
+# blank lines and lines that start with a single # are ignored
+
+function file2checkbox ($field_name)
+{
+	$field[$field_name] = $GLOBALS[field][$field_name];
+	if(is_array ($field[$field_name]))
+		foreach ($field[$field_name] as $item)
+			$checked[$item] = ' checked';
+
+	foreach (file ($field_name.'_list.txt') as $temp)
+	{
+		$temp = trim ($temp);
+		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
+
+		list ($value, $option) = explode ('::', $temp);
+		$option
+			or $option = $value;
+		$output .= sprintf ('<input type="checkbox" name="field[%s]" value="%s"%s /> %s<br />'."\n", $field_name, $value, $selected[$value], $option);
+	}
+
+	return "\n$output";
+}
+
+function file2cb_column ($field_name)
+{
+	$field[$field_name] = $GLOBALS[field][$field_name];
+
+	if(is_array ($field[$field_name]))
+		foreach ($field[$field_name] as $item)
+			$checked[$item] = ' checked';
+
+	$modules = file ($field_name.'_list.txt');
+	$no_of_cells = count ($modules);
+	$row_length  = ceil ($no_of_cells/3);
+
+
+
+	for ($n=0, $col = 0, $row = 0; $n < $no_of_cells; ++$n)
+	{
+		$temp = trim ($modules[$n])
+			or $temp = '&nbsp';
+
+		if (! $temp || $temp[0] == '#') continue;	# ignore blanks and comments
+
+		list ($value, $option) = explode ('::', $temp);
+
+		$option
+			or $option = $value;
+
+		$line  = '<td><font face="Tahoma, Arial, Helvetica, sans-serif" SIZE="2">';
+		$line .= sprintf ('<input type="checkbox" name="field[%s][]" value="%s" %s /> %s ', $field_name, $value, $checked[$value], $option);
+		$line .= '</font></td>';
+
+		$cell[$col][$row] = $line;
+
+		++$row;
+		if ($row == $row_length)
+		{
+			$row = 0;
+			++$col;
+		}
+	}
+
+	for ($row = 0; $cell[0][$row]; ++$row)
+		$out .= '<tr bgcolor="#F0F0F0">'.$cell[0][$row].$cell[1][$row].$cell[2][$row].'</tr>';
+
+	return '<table border="1" width="100%">'.$out.'</table>';
 }
 
 # generate_row
@@ -42,11 +131,13 @@ function generate_row ($column_data, $no_of_columns = 2)
 		$row .= '</font></td>';
 	}
 
-	$row .= '</tr>';
-
-	return $row;
+	return $row . '</tr>';
 }
 
-
-
+function I_am_so_embarrassed ($group_name, $name, $note_desc = 'Notes', $long_note=0)
+{
+		  	list ($version_field, $note_desc, $note_field) = widget ($group_name, $name, $note_desc, $long_note);
+			print generate_row (array ($name, $version_field));
+			print generate_row (array ($note_desc, $note_field));
+}
 ?>
