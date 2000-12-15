cb97a6b608cf4a916c37c9367f4fd8eb78a18cdb
Zak Greant <zak@php.net>
2000-12-15 16:40:37 +0000

    Updating php|qat membership list. If anyone disagrees with these changes, please let me know - and make sure to CC the list in the message.  --zak

--- members.php
+++ members.php
@@ -7,9 +7,8 @@
 
     <h1>PHP|QAT Members</h1>
     <p>
-      <b>If you would like to become a member,</b> please send email
-      to the <a href="mailto:PHP-qa@lists.PHP.net">PHP|QAT Mailing
-      List</a>.
+      <b>To become a member,</b> you need to contribute to the PHP Quality Assurance effort.
+      We always need people to hunt for bugs, to test Release Candidates and otherwise help out. Start by subscribing to the PHP|QA mailing list (Send a blank message to <a href="mailto:php-qa-subscribe@lists.php.net">php-qa-subscribe@lists.php.net</a>. Once you have subscribed, sent a message to the list introducing yourself.  We would like to know your name, your skill set and your interests (as they relate to doing QA for PHP).<br />Every once in a while core QA team members review and update the membership list.  If you have been actively contributing to the effort for a while, we may ask you to join the group. Also, you can mail the group and ask to become a member.
     </p>
 
     <p>
@@ -19,30 +18,125 @@
       <li>Your name</li>
       <li>A short list of your skills</li>
       <li>What PHP|QAT projects or goals you would like to work on.</li>
-      <li>A rough estimate of how much time you are able to commit to the project.</li>
     </ul>
 
-    <h2>Current Members</h2>
+    <h2>PHP|QAT Members</h2>
 
     <ul>
-      <li><b>Alexander Feldman</b>&lt;<a href="mailto:alex@zend.com">alex@zend.com</a>&gt;
+      <li><b>Andr&eacute; Langhorst </b> &lt;<a href="mailto:waldschrott@php.net">waldschrott@php.net</a>&gt;
         <ul>
-          <li>Skills: C/C++</li>
-          <li>Focus: Help for the design of the QA suite, ideas, coding, scripts</li>
+          <li>Skills: PHP, Java, Javascript, XML, XSL, ODBC (Adabas,Mysql,PostgreSQL,Oracle(8)), HTML</li>
+          <li>Focus: Closing bugs, QA for PHP Release Candidate</li>
+          <li>Platform 1: Windows 2000 Apache + Latest PHP + MySQL + Adabas (ODBC)</li>
+          <li>Platform 2: Linux + PHP (module) + MySQL + Adabas (ODBC) + PostgreSQL</li>
         </ul>
       </li>
     </ul>
 
     <ul>
-      <li><b>Andreas Otto</b> &lt;<a href="mailto:a-otto@web.de">a-otto@web.de</a>&gt;
+      <li><b>Hellekin O. Wolf</b> &lt;<a href="mailto:hellekin@imaginet.fr">hellekin@imaginet.fr</a>&gt;
         <ul>
-          <li>Skills: Web development using PHP</li>
-          <li>Focus: Testing release candidates</li>
-          <li>Platform 1: SUSE Linux 6.4 + Apache 1.3.12 + mod_PHP</li>
-          <li>Platform 2: WinNT 4 (Service Pack 5) + Apache 1.3.12 + mod_PHP</li>
+          <li>Skills: Web development and design, Team management, Internet culture, French</li>
+          <li>Focus: PHP for multi-hosting (aka, providers issues), ideas, code, bug hunting</li>
+          <li>Platform: Linux(es) (some FreeBSD available also)</li>
         </ul>
       </li>
     </ul>
+
+    <ul>
+      <li><b>Jalal Pushman</b> &lt;<a href="mailto:the_jalal@yahoo.com">the_jalal@yahoo.com</a>&gt;
+        <ul>
+          <li>Skills: C++, Java, Perl &amp; PHP</li>
+          <li>Focus: Closing bugs, Developing automated testing systems</li>
+          <li>Platform 1: SuSE Linux 6.3 + Apache + MySQL + PHP3 + PHP4</li>
+          <li>Platform 2: Windows NT + Apache + PHP 4</li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>James Moore</b> &lt;<a href="mailto:jmoore@php.net">jmoore@php.net</a>&gt;
+        <ul>
+          <li>Skills: Web Development with PHP, Some C/C++</li>
+          <li>Focus: Windows Builds and Testing</li>
+          <li>Platform 1: Redhat 6.2 thttpd/Apache + PHP4 + MySQL</li>
+          <li>Platform 2: Windows 98 + Apache + MySQL</li>
+          <li>Platform 3: Windows 2000 + IIS5 (ISAPI &amp; CGI) + MySQL</li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>Jani Taskinen</b> &lt;<a href="mailto:sniper@php.net">sniper@php.net</a>&gt;
+        <ul>
+          <li>Skills: Linux, Solaris (sparc), PHP, MySQL, LDAP, some C, some Perl</li>
+          <li>Focus: Bug hunting/fixing, build testing (almost every day) </li>
+          <li>Platform 1: RH Linux 6.2 (i386)</li>
+          <ul>
+             <li>Apache 1.3.14 + PHP 4.x.x-CVS + almost every extension (varies)</li>
+             <li>All compiled with egcs-2.91.66</li>
+          </ul>
+          <li>Platform 2: Solaris (sparc) 2.6</li>
+          <ul>
+             <li>Apache 1.3.12 + PHP 4.x.x-CVS + almost every extension (varies)</li>
+             <li>All compiled with Sun CC (WorkShop Compilers 5.0 98/12/15 C 5.0)</li>
+          </ul>
+          <li>Platform 3: Solaris 8 (Sparc)
+          <ul>
+             <li>Apache 1.3.12 + PHP 4.0.4-dev + LDAP + XML (expat)</li>
+             <li>Compiled with gcc-2.95.2</li>
+          </ul>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>Joey Smith</b> &lt;<a href="mailto:joey@php.net">joey@php.net</a>&gt;
+        <ul>
+          <li>Skills: C, Perl, PHP, Sybase (DB and CT libs)</li>
+          <li>Focus: Developing automated testing systems, testing releases, committing patches, closing bugs, follow up with users</li>
+          <li>Platform 1: Debian Linux (2.1, 2.2) - CGI/Apache Module, Sybase, Mysql, PDFLib, GD, DOM/XML/expat, IMAP</li>
+          <li>Platform 2: Solaris 2.6 - modules as platform 1</li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>Olivier Cahagne</b> &lt;<a href="mailto:olivier.cahagne@epita.fr">olivier.cahagne@epita.fr</a>&gt;
+        <ul>
+          <li>Skills: C, Perl, PHP, MySQL, Apache, etc...</li>
+		  <li>Environment: php scripts run by hundreds of users in a computer science school that has to be secure (so, running suEXEC).</li>
+          <li>Focus: building/testing snapshots. Bug hunting, ideas, suggestions. Make sure that "old" gcc and make can still compile PHP. :)</li>
+          <li>Platform 1: NetBSD 1.3.3 (i386), gcc 2.7.2.2, cgi+suEXEC. Modules: imap, curl, mhash, mcrypt, ftp, sockets</li>
+          <li>Platform 2: OpenBSD 2.6 (i386), gcc 2.95.2, cgi</li>
+          <li>Platform 3: Linux RH6.2 (i386), egcs 2.91.66, cgi</li>
+          <li>Platform 4: Digital Unix 4.0 (alpha), gcc 2.7.2.3, cgi</li>
+          <li>Platform 5: NetBSD 1.4.2 (i386), egcs 2.90.66, cgi</li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>Phil Driscoll</b> &lt;<a href="mailto:phil@dialsolutions.co.uk">phil@dialsolutions.co.uk</a>&gt;
+        <ul>
+          <li>Skills: C, PHP, BASIC, ARM Assembler</li>
+          <li>Focus: Build and Test Release Candidates</li>
+          <li>Platforms: NT, Risc OS, Incompetent Linux user (probably useful!)</li>
+        </ul>
+      </li>
+    </ul>
+
+    <ul>
+      <li><b>Zak Greant</b> &lt;<a href="mailto:zak@php.net">zak@php.net</a>&gt;
+        <ul>
+          <li>Skills: Graphic Design, PHP, Project Management, MySQL, etc...</li>
+          <li>Focus: Coordinating the PHP|QAT effort, Developing PHP|QAT resources, Closing bugs</li>
+        </ul>
+      </li>
+    </ul>
+
+
+    <h2>PHP|QAT Supporting Members</h2>
 
     <ul>
       <li><b>Brendan W. McAdams</b> &lt;<a href="mailto:brendan@plexmedia.com">brendan@plexmedia.com</a>&gt;
@@ -68,30 +162,10 @@
     </ul>
 
     <ul>
-      <li><b>Evan Klein</b> &lt;<a href="mailto:HQ@linux-guruz.com">HQ@linux-guruz.com</a>&gt;
-        <ul>
-          <li>Skills: Linux, PHP, Perl and MySQL</li>
-          <li>Focus: Bug Hunting, QA Team Website and Apps</li>
-          <li>Platform: ?</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
       <li><b>Hartmut Holzgraefe</b> &lt;<a href="mailto:harmut@six.de">harmut@six.de</a>&gt;
         <ul>
           <li>Skills: C/C++, Java, PHP, autoconf, MySQL, Adabas, Oracle, and so on...</li>
           <li>Focus: fixing bugs, review of config.m4 implementation in different modules</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Hellekin O. Wolf</b> &lt;<a href="mailto:hellekin@imaginet.fr">hellekin@imaginet.fr</a>&gt;
-        <ul>
-          <li>Skills: Web development and design, Team management, Internet culture, French</li>
-          <li>Focus: PHP for multi-hosting (aka, providers issues), ideas, code, bug hunting</li>
-          <li>Platform: Linux(es) (some FreeBSD available also)</li>
         </ul>
       </li>
     </ul>
@@ -106,54 +180,7 @@
       </li>
     </ul>
 
-    <ul>
-      <li><b>jalal</b> &lt;<a href="mailto:the_jalal@yahoo.com">the_jalal@yahoo.com</a>&gt;
         <ul>
-          <li>Skills: C++, Java, Perl &amp; PHP</li>
-          <li>Focus: Closing bugs, Developing automated testing systems</li>
-          <li>Platform 1: SuSE Linux 6.3 + Apache + MySQL + PHP3 + PHP4</li>
-          <li>Platform 2: Windows NT + Apache + PHP 4</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>James Moore</b> &lt;<a href="mailto:jmoore@php.net">jmoore@php.net</a>&gt;
-        <ul>
-          <li>Skills: Web Development with PHP, Some C/C++</li>
-          <li>Focus: Windows Builds and Testing</li>
-          <li>Platform 1: Redhat 6.2 thttpd/Apache + PHP4 + MySQL, </li>
-          <li>Platform 2: Windows 98 + Apache + MySQL</li>
-          <li>Platform 3: Windows 2000 + IIS5 (ISAPI &amp; CGI) + MySQL</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Joey Smith</b> &lt;<a href="mailto:joey@php.net">joey@php.net</a>&gt;
-        <ul>
-          <li>Skills: C, Perl, PHP, Sybase (DB and CT libs)</li>
-          <li>Focus: Developing automated testing systems, testing releases, committing patches, closing bugs, follow up with users</li>
-          <li>Platform 1: Debian Linux (2.1, 2.2) - CGI/Apache Module, Sybase, Mysql, PDFLib, GD, DOM/XML/expat, IMAP</li>
-          <li>Platform 2: Solaris 2.6 - modules as platform 1</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Karl Austin</b> &lt;<a href="mailto:karl@kdawebservices.com">karl@kdawebservices.com</a>&gt;
-        <ul>
-          <li>Skills: PHP, HTML, JavaScript, Visual Basic, MySQL, Graphic Design</li>
-          <li>Focus: Advocacy, QA Team website/app developement, Testing - some building </li>
-          <li>Platform 1: Windows 2000 + Apache 1.3.12 + PHP4</li>
-          <li>Platform 2: Windows 98 + Apache 1.3.12 + PHP4</li>
-          <li>Platform 3: RedHat 5.1 (very customized) + PHP 3.0.12 + Apache 1.3.9</li>
-          <li>Platform x: May have access to Red Hat 6.1 / PHP 4 and Win ME soon.</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
       <li><b>Kirill Maximov</b> &lt;<a href="mailto:maxkir@email.com">maxkir@email.com</a>&gt;
         <ul>
           <li>Skills: C/C++, Java, PHP</li>
@@ -163,65 +190,7 @@
       </li>
     </ul>
 
-    <ul>
-      <li><b>Mårten Gustafson</b> &lt;<a href="mailto:marten@jerbro.se">marten@jerbro.se</a>&gt;
         <ul>
-          <li>Skills: PHP, DBMaker</li>
-          <li>Focus: Build and Test PHP, occasional bug hunting</li>
-          <li>Platform 1: Debian Linux + Apache + mod_PHP + DBMaker</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Olivier Cahagne</b> &lt;<a href="mailto:olivier.cahagne@epita.fr">olivier.cahagne@epita.fr</a>&gt;
-        <ul>
-          <li>Skills: C, Perl, PHP, MySQL, Apache, etc...</li>
-		  <li>Environment: php scripts run by hundreds of users in a computer science school that has to be secure (so, running suEXEC).</li>
-          <li>Focus: building/testing snapshots. Bug hunting, ideas, suggestions. Make sure that "old" gcc and make can still compile PHP. :)</li>
-          <li>Platform 1: NetBSD 1.3.3 (i386), gcc 2.7.2.2, cgi+suEXEC. Modules: imap, curl, mhash, mcrypt, ftp, sockets</li>
-          <li>Platform 2: OpenBSD 2.6 (i386), gcc 2.95.2, cgi</li>
-          <li>Platform 3: Linux RH6.2 (i386), egcs 2.91.66, cgi</li>
-          <li>Platform 4: Digital Unix 4.0 (alpha), gcc 2.7.2.3, cgi</li>
-          <li>Platform 5: NetBSD 1.4.2 (i386), egcs 2.90.66, cgi</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Patrik Bengtsson</b> &lt;<a href="mailto:patrik.bengtsson@grafix.se">patrik.bengtsson@grafix.se</a>&gt;
-        <ul>
-          <li>Skills: PHP, SQL, HTTP on various platforms (Mac, Win, Linux).</li>
-          <li>Focus: Build and Test Releases</li>
-          <li>Platform 1: MacOS X</li>
-          <li>Platform 2: WinNT with IIS</li>
-          <li>Modules: PDFlib and MySQL.</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Phil Driscoll</b> &lt;<a href="mailto:phil@dialsolutions.co.uk">phil@dialsolutions.co.uk</a>&gt;
-        <ul>
-          <li>Skills: C, PHP, BASIC, ARM Assembler</li>
-          <li>Focus: Build and Test Release Candidates</li>
-          <li>Platforms: NT, Risc OS, Incompetent Linux user (probably useful!)</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Rebecca &quot;Bean&quot; Visger</b> &lt;<a href="mailto:rvisger@sjmarketing.com">rvisger@sjmarketing.com</a>&gt;
-        <ul>
-          <li>Skills: Web Development, PHP, mySQL, HTML, javascript, design, etc...</li>
-          <li>Platform 1: FreeBSD 3.4-RELEASE</li>
-          <li>Platform 2: FreeBSD 4.0</li>
-          <li>Platform 3: BSDI BSD/OS 4.0.1</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
       <li><b>Richard Lynch</b> &lt;<a href="mailto:richard@zend.com">richard@zend.com</a>&gt;
         <ul>
           <li>Skills: PHP, Lisp and others I refuse to name for fear of head-hunters</li>
@@ -241,34 +210,15 @@
       </li>
     </ul>
 
+    <h2>PHP|QAT Inactive Members</h2>
     <ul>
-      <li><b>Shawn Wallace</b> &lt;<a href="mailto:lists@wa.ca">lists@wa.ca</a>&gt;
-        <ul>
-          <li>Skills: C/C++, Java, PHP, GD, Perl, MySQL, DB2, and so on...</li>
-          <li>Focus: Closing bugs, testing pre-releases, assisting with QAT resource development</li>
-          <li>Platform: Debian Linux + Apache-SSL + MySQL</li>
-        </ul>
-      </li>
+      <li>Alexander Feldman</li>
+      <li>Andreas Otto</li>
+      <li>Evan Klein</li>
+      <li>Karl Austin</li>
+      <li>Mårten Gustafson</li>
+      <li>Patrik Bengtsson</li>
+      <li>Rebecca "Bean" Visger</li>
+      <li>Shawn Wallace</li>
     </ul>
-
-    <ul>
-      <li><b>Andr&eacute; Langhorst </b> &lt;<a href="mailto:waldschrott@php.net">waldschrott@php.net</a>&gt;
-        <ul>
-          <li>Skills: PHP, Java, Javascript, XML, XSL, ODBC (Adabas,Mysql,PostgreSQL,Oracle(8)), HTML</li>
-          <li>Focus: Closing bugs, QA for PHP Release Candidate</li>
-          <li>Platform 1: Windows 2000 Apache + Latest PHP + MySQL + Adabas (ODBC)</li>
-          <li>Platform 2: Linux + PHP (module) + MySQL + Adabas (ODBC) + PostgreSQL</li>
-        </ul>
-      </li>
-    </ul>
-
-    <ul>
-      <li><b>Zak Greant</b> &lt;<a href="mailto:zak@php.net">zak@php.net</a>&gt;
-        <ul>
-          <li>Skills: Graphic Design, PHP, Project Management, MySQL, etc...</li>
-          <li>Focus: Coordinating the PHP|QAT effort, Developing PHP|QAT resources, Closing bugs</li>
-        </ul>
-      </li>
-    </ul>
-
 <? include ('inc/footer.inc') ?>
