1e7dfaded435297387ec445751a009d82af0dd1b
Marco Kaiser <bate@php.net>
2001-10-17 22:49:54 +0000

    *** empty log message ***

---
+++ preview/index.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+<title>PHP-QAT: Quality Assurance Team</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" link="#000000" vlink="#000000" alink="#000000" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="70" background="gfx/orange_light.jpg">
+  <tr>
+    <td width="130"><img src="gfx/spacer.gif" width="3" height="1"><img src="gfx/logo_qa.jpg" width="111" height="58"></td>
+    <td valign="middle" align="right">
+      <table width="500" border="0" cellspacing="0" cellpadding="0" height="70">
+        <tr>
+          <td align="right" valign="top" class="headline_white">[%HEAD_DATE%]</td>
+        </tr>
+        <tr>
+          <td valign="bottom" align="right" class="head_links">Home | Projects 
+            and Goals | Links &amp; Resources | <a href="members.php">Members</a> 
+            | <a href="howtohelp.php">How to help</a></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="22">
+  <tr> 
+    <td bgcolor="#000000"><img src="gfx/spacer.gif" width="1" height="1"></td>
+  </tr>
+  <tr>
+    <td background="gfx/orange_dark.jpg">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#000000"><img src="gfx/spacer.gif" width="1" height="1"></td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="81%">
+  <tr>
+    <td width="200" background="gfx/grey_light.jpg" align="left" valign="top"> 
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td width="5"><img src="gfx/spacer.gif" width="5" height="1"></td>
+          <td>&nbsp;</td>
+          <td width="5"><img src="gfx/spacer.gif" width="5" height="1"></td>
+        </tr>
+        <tr> 
+          <td width="5">&nbsp;</td>
+          <td class="headline_grey">PHP-QAT?</td>
+          <td width="5">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td width="5">&nbsp;</td>
+          <td class="head_links">The PHP/QA Team is a small group of developers 
+            whose primary goal is to support the PHP core developers by providing 
+            them with timely quality assurance.</td>
+          <td width="5">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td width="5">&nbsp;</td>
+          <td class="head_links">&nbsp;</td>
+          <td width="5">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td width="5">&nbsp;</td>
+          <td class="headline_grey">Members?</td>
+          <td width="5">&nbsp;</td>
+        </tr>
+        <tr>
+          <td width="5">&nbsp;</td>
+          <td class="subhead_news">View the PHP-QAT Member register. (<a href="members.php">view</a>)</td>
+          <td width="5">&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+    <td background="gfx/line_1.jpg" width="1"><img src="gfx/spacer.gif" width="1" height="1"><br>
+    </td>
+    <td align="left" valign="top"> <br>
+      <table width="70%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
+          <td width="100%" class="headline_orange">Welcome to the<br>
+            PHP Quality Assurance Team Web Page. </td>
+          <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
+        </tr>
+        <tr> 
+          <td width="10">&nbsp;</td>
+          <td width="100%">&nbsp;</td>
+          <td width="10">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td width="10">&nbsp;</td>
+          <td width="100%"> 
+            <p>The team is currently focused on several areas: </p>
+            <ul>
+              <li>Reworking the bug tracking system. (Subscribe to to <a href="mailto:jitterbug-subscribe@lists.php.net">jitterbug-subscribe@lists.php.net</a> 
+                for more information)</li>
+              <li>Processing the open bug reports from the official PHP 4 bug 
+                list (<a href="http://bugs.php.net" target="_blank">http://bugs.php.net</a>).</li>
+              <li>Providing QA for the latest PHP release candidate.</li>
+            </ul>
+          </td>
+          <td width="10">&nbsp;</td>
+        </tr>
+        <tr>
+          <td width="10">&nbsp;</td>
+          <td width="100%">If you would like to contribute to this effort, please 
+            visit our How To Help page. (<a href="howtohelp.php">view</a>)</td>
+          <td width="10">&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="22">
+  <tr> 
+    <td bgcolor="#000000"><img src="gfx/spacer.gif" width="1" height="1"></td>
+  </tr>
+  <tr> 
+    <td background="gfx/orange_dark.jpg">&nbsp;</td>
+  </tr>
+  <tr> 
+    <td bgcolor="#000000"><img src="gfx/spacer.gif" width="1" height="1"></td>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" height="60">
+  <tr> 
+    <td background="gfx/grey_dark.jpg" valign="top" align="left" class="head_links" width="50%"><a href="http://www.php.net/copyright.php" target="_blank">Copyright 
+      &copy; 2001 The PHP Group</a><br>
+      All rights reserved.</td>
+    <td background="gfx/grey_dark.jpg" class="head_links" width="50%" valign="top" align="right">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
