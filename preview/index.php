88be0472fd0cc02033896b82a295b3caae2cad33
Marco Kaiser <bate@php.net>
2001-10-21 01:19:52 +0000

    Added Template Class and started function list.

--- preview/index.php
+++ preview/index.php
@@ -1,2 +1,13 @@
 <?php
+include("include/freeTemplate.class.php");
+include("include/functions.php");
+
+$qa_cl = new freeTemplate;
+
+	$qa_cl->assignTemplate($template_array["home"]);
+	replace_default($qa_cl);
+	
+	$qa_cl->printTemplate();
+
+unset($qa_cl);
 ?>
