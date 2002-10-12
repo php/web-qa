c2cd1e9ffe86ffe00f784367df144e216218b330
Derick Rethans <derick@php.net>
2002-10-12 20:07:06 +0000

    - Typo

--- buildtest-process.php
+++ buildtest-process.php
@@ -1,5 +1,5 @@
 <?php
-	if (!isset($_POST['php_test_data'])) {
+	if (isset($_POST['php_test_data'])) {
 		mail ("php-qa@lists.php.net", "Test results", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
 		mail ("derick@php.net", "Test results", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
 	}
