4928df581da1da15cedb77a88f46955a54e1290e
jim winstead <jimw@php.net>
2002-12-16 22:50:30 +0000

    don't even try to mail reports that are too big to make it through the list

--- buildtest-process.php
+++ buildtest-process.php
@@ -1,5 +1,8 @@
 <?php
 	if (isset($_POST['php_test_data'])) {
+		if (strlen($_POST['php_test_data']) > 90000) {
+			die("can't handle input that large.");
+		}
 		mail ("php-qa@lists.php.net", "Test results", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
 	}
 ?>
