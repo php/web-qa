72cbf1f7c57f93ab10eca0b996839c3351318d85
Derick Rethans <derick@php.net>
2002-12-17 18:58:00 +0000

    - Report failed and successful tests in subject

--- buildtest-process.php
+++ buildtest-process.php
@@ -3,7 +3,21 @@
 		if (strlen($_POST['php_test_data']) > 90000) {
 			die("can't handle input that large.");
 		}
-		mail ("php-qa@lists.php.net", "Test results", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
+		if (isset($_GET['status'])) {
+			switch($_GET['status']) {
+				case 'failed':
+					$status = "failed";
+					break;
+				case 'success':
+					$status = "success";
+					break;
+				default:
+					$status = "unknown";
+			}
+		} else {
+			$status = "unknown";
+		}
+		mail ("php-qa@lists.php.net", "Test results [$status]", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
 	}
 ?>
 $Revision$
