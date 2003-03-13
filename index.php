32fd5f3c34a908df6535d66e66d46689195f3597
foobar <sniper@php.net>
2003-03-13 15:55:03 +0000

    xhtml..*sigh*

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("include/functions.php");
 
 $TITLE = "PHP-QAT: Quality Assurance Team";
-$SITE_UPDATE = date("D M d H:i:s Y T", filectime($SCRIPT_FILENAME))."<br>
+$SITE_UPDATE = date("D M d H:i:s Y T", filectime($SCRIPT_FILENAME))."<br />
 /* $Id$ */";
 
 common_header();
@@ -11,7 +11,7 @@ common_header();
         <tr> 
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
           <td width="100%"> 
-           <h1>Welcome to the<br>PHP Quality Assurance Team Web Page.</h1>
+           <h1>Welcome to the<br />PHP Quality Assurance Team Web Page.</h1>
            <p>
             The PHP Quality Assurance Team supports the PHP Development Team by 
             providing them with information on compatibility and stability issues.
@@ -45,7 +45,7 @@ common_header();
         <tr> 
           <td width="10">&nbsp;</td>
           <td width="100%">
-          <br>
+          <br />
           <p>
            If you would like to contribute to these efforts, please 
            visit our <a href="howtohelp.php">How To Help</a> page.
