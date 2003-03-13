c90b647317dddbd1bafa2fee23db374a49ee1e3d
foobar <sniper@php.net>
2003-03-13 15:29:17 +0000

    Announce the 4.3.2RC1 testing..

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 include("include/functions.php");
 
 $TITLE = "PHP-QAT: Quality Assurance Team";
-$SITE_UPDATE = date("D M d H:i:s Y T", filectime($SCRIPT_FILENAME));
-/* $Id$ */
+$SITE_UPDATE = date("D M d H:i:s Y T", filectime($SCRIPT_FILENAME))."<br>
+/* $Id$ */";
 
 common_header();
 ?>
@@ -11,24 +11,45 @@ common_header();
         <tr> 
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
           <td width="100%"> 
-            <h1>Welcome to the<br />PHP Quality Assurance Team Web Page</h1>
-
+           <h1>Welcome to the<br>PHP Quality Assurance Team Web Page.</h1>
+           <p>
+            The PHP Quality Assurance Team supports the PHP Development Team by 
+            providing them with information on compatibility and stability issues.
+           </p>
+          </td>
+          <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
+        </tr>
+        <tr> 
+          <td width="10">&nbsp;</td>
+          <td width="100%"> 
+            <p><b>The team is currently focused on:</b></p>
+            <ul>
+              <li>
+                <span class="lihack">Providing QA for the PHP 4.3.2RC1 
+                (<a href="http://www.php.net/~jani/RC/php-4.3.2RC1.tar.gz">source</a>, 
+                 <a href="http://www.php.net/~jani/RC/php-4.3.2RC1-Win32.zip">windows binaries</a>) release.
+                </span><br />
+              </li>
+              <li>
+                Upgrading the testsuite. This includes extending the testsuites with tests for every function, 
+                collecting test data in a different way then mailing all output to the QA mailinglist.
+              </li>
+            </ul>
             <p>
-              The PHP Quality Assurance Team is supporting to the PHP
-              Development Team by means of providing them with information on
-              compatibility and stability issues.
-            </p>
-            <p>
-              The team is currently focused on upgrading the testsuite. This
-              includes extending the testsuites with tests for every function,
-              collecting test data in a different way then mailing all output
-              to the QA mailinglist.
-            </p>
-            <p>
-              If you would like to contribute to this effort, please visit our
-              <a href="howtohelp.php">How To Help</a> page.
+             Anyone can help us by running the test framework, see:<br />
+             <a href="http://qa.php.net/running-tests.php">http://qa.php.net/running-tests.php</a>
             </p>
           </td>
+          <td width="10">&nbsp;</td>
+        </tr>
+        <tr> 
+          <td width="10">&nbsp;</td>
+          <td width="100%">
+          <p>
+           If you would like to contribute to these efforts, please 
+           visit our <a href="howtohelp.php">How To Help</a> page.
+          </p>
+		</td>
           <td width="10">&nbsp;</td>
         </tr>
         <tr> 
