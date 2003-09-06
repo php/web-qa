51f44bf97828f7afa43025a2580aee665b684e44
foobar <sniper@php.net>
2003-09-06 12:38:13 +0000

    show yearly stats instead of current month

--- stats.php
+++ stats.php
@@ -7,12 +7,15 @@ $SITE_UPDATE = date("D M d H:i:s Y T", filectime($SCRIPT_FILENAME));
 /* $Id$ */
 
 common_header();
+
+$year = date ('Y');
+
 ?>
 	<table width="70%" border="0" cellspacing="0" cellpadding="0">
         <tr> 
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
           <td width="100%"> 
-            <h1>Statistics</h1>
+            <h1>Statistics <?php echo $year; ?></h1>
           </td>
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
         </tr>
@@ -20,7 +23,7 @@ common_header();
           <td width="10">&nbsp;</td>
           <td width="100%">
            <?php include('http://www.php.net/~jani/bugsmap001'); ?>
-           <img src="http://www.php.net/~jani/count-<?php echo date ('Y-m'); ?>.png" ISMAP USEMAP="#bugsmap001" border="0" alt=""><br /><br />
+           <img src="http://www.php.net/~jani/total-count-<?php echo $year; ?>.png" ISMAP USEMAP="#bugsmap001" border="0" alt=""><br /><br />
           </td>
           <td width="10">&nbsp;</td>
         </tr>
