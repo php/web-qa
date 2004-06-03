d466927f3c78f7b348e2fbc810d10991c5ead731
Ilia Alshanetsky <iliaa@php.net>
2004-06-03 12:32:31 +0000

    4.3.7 release.

--- include/release-qa.php
+++ include/release-qa.php
@@ -7,7 +7,7 @@
 
 $CURRENT_QA_RELEASE = '4.3.7RC1';
 $BUILD_TEST_RELEASES = array('4.3.6', '4.3.7RC1', '5.0.0RC2', '5.0.0RC3-dev');
-$RELEASE_PROCESS = true;
+$RELEASE_PROCESS = false;
 
 $RC_FILES = array (
 	array (	
