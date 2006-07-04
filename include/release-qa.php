568d1cc1e88c6fab2c8a40ce711ad5a7145aeb01
Derick Rethans <derick@php.net>
2006-07-04 16:52:33 +0000

    - Updated for new RC.

--- include/release-qa.php
+++ include/release-qa.php
@@ -5,8 +5,8 @@
  *  with list of urls to the packages.
  */
 
-$CURRENT_QA_RELEASE = "4.4.3RC1";
-$BUILD_TEST_RELEASES = array( '4.4.3RC1' /*, '5.1.3RC3'*/ );
+$CURRENT_QA_RELEASE = "4.4.3RC2";
+$BUILD_TEST_RELEASES = array( '4.4.3RC2' /*, '5.1.3RC3'*/ );
 $RELEASE_PROCESS = true;
 $RC_FILES = array (
 	array (	
