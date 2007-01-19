58c6ad21d8bfab28272b789a562cda9b1efbcdf2
Derick Rethans <derick@php.net>
2007-01-19 15:22:03 +0000

    - Remove the urldecode for tony.

--- buildtest-process.php
+++ buildtest-process.php
@@ -25,7 +25,7 @@
 			$version = "unknown";
 		}
 		if (in_array($version, $BUILD_TEST_RELEASES)) {
-			mail ("php-qa@lists.php.net", "Test results for $version [$status]", base64_decode(urldecode($_POST['php_test_data'])), "From: noreply@php.net");
+			mail ("php-qa@lists.php.net", "Test results for $version [$status]", base64_decode($_POST['php_test_data']), "From: noreply@php.net");
 		}
 	}
 ?>
