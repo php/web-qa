3f31cda63ea5eaf5289dab1b04629644aac231df
Ilia Alshanetsky <iliaa@php.net>
2007-01-26 00:20:10 +0000

    5.2.1RC4

--- include/release-qa.php
+++ include/release-qa.php
@@ -6,7 +6,7 @@
  */
 
 $CURRENT_QA_RELEASE = "4.4.5RC1";
-$BUILD_TEST_RELEASES = array( '4.4.5RC1' , '5.2.1RC3');
+$BUILD_TEST_RELEASES = array( '4.4.5RC1' , '5.2.1RC4');
 $RELEASE_PROCESS = true;
 $RC_FILES = array (
 	array (	
@@ -26,7 +26,7 @@ $RC_FILES = array (
 );
 
 /* PHP 5 Releases */
-$CURRENT_QA_RELEASE_5 = '5.2.1RC3';
+$CURRENT_QA_RELEASE_5 = '5.2.1RC4';
 $RC_FILES_5 = array (
 
 	array (	
