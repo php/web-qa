6c95494fe670c508e599c540e186eeb45f117394
Ilia Alshanetsky <iliaa@php.net>
2007-05-25 00:15:50 +0000

    5.2.3RC1

--- include/release-qa.php
+++ include/release-qa.php
@@ -5,7 +5,7 @@
  *  with list of urls to the packages.
  */
 
-$BUILD_TEST_RELEASES = array( '4.4.7', '5.2.2');
+$BUILD_TEST_RELEASES = array( '4.4.7', '5.2.3RC1');
 $DEV_RELEASES = array();
 
 foreach($BUILD_TEST_RELEASES as $release) {
@@ -15,7 +15,7 @@ foreach($BUILD_TEST_RELEASES as $release) {
 	$DEV_RELEASES[] = $release . "-dev";
 }
 
-$RELEASE_PROCESS = array(4 => false, 5 => false);
+$RELEASE_PROCESS = array(4 => false, 5 => true);
 
 $CURRENT_QA_RELEASE_4 = false; //"4.4.7RC1"; 
 $RC_FILES_4 = array (
@@ -34,7 +34,7 @@ $RC_FILES_4 = array (
 );
 
 /* PHP 5 Releases */
-$CURRENT_QA_RELEASE_5 = false; //'5.2.2RC2';
+$CURRENT_QA_RELEASE_5 = '5.2.3RC1';
 $RC_FILES_5 = array (
 
 	array (	
