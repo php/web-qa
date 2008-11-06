69f329e913f221b01dce71ae0be5a7aa51574fdf
Ilia Alshanetsky <iliaa@php.net>
2008-11-06 22:17:58 +0000

    5.2.7RC3

--- include/release-qa.php
+++ include/release-qa.php
@@ -5,7 +5,7 @@
  *  with list of urls to the packages.
  */
 
-$BUILD_TEST_RELEASES = array('5.3.0alpha2', '5.2.7RC2');
+$BUILD_TEST_RELEASES = array('5.3.0alpha2', '5.2.7RC3');
 $DEV_RELEASES = array();
 
 foreach($BUILD_TEST_RELEASES as $release) {
@@ -17,7 +17,7 @@ foreach($BUILD_TEST_RELEASES as $release) {
 
 $RELEASE_PROCESS = array(52 => true, 53 => true);
 
-$CURRENT_QA_RELEASE_52 = '5.2.7RC2';
+$CURRENT_QA_RELEASE_52 = '5.2.7RC3';
 $RC_FILES_52 = array (
 	array (
 		'http://downloads.php.net/ilia/',
