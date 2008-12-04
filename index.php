aa668d4dd30dd30660d4692c68fa32c4257b8585
Lukas Smith <lsmith@php.net>
2008-12-04 21:13:36 +0000

    point to the windows.php.net qa instead of the downloads site

--- index.php
+++ index.php
@@ -10,35 +10,35 @@ common_header();
 
 ?>
       <table width="70%" border="0" cellspacing="0" cellpadding="0">
-        <tr> 
+        <tr>
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
-          <td width="100%"> 
+          <td width="100%">
            <h1>Welcome to the<br />PHP Quality Assurance Team Web Page.</h1>
            <p>
-            The PHP Quality Assurance Team supports the PHP Development Team by 
+            The PHP Quality Assurance Team supports the PHP Development Team by
             providing them with information on compatibility and stability issues.
            </p>
           </td>
           <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
         </tr>
-        <tr> 
+        <tr>
           <td width="10">&nbsp;</td>
-          <td width="100%"> 
+          <td width="100%">
             <br /><h3>The team is currently focused on:</h3>
             <ul>
 
-             <li>
+              <li>
 <?php show_release_qa(); ?>
-				
-				See <a href='http://windows.php.net/download/'>here</a> for the Windows builds.
-				<br><br>
-				
-             </li>
 
-             <li>
-              Upgrading the testsuite. This includes extending the testsuites with tests for every function, 
-              collecting test data in a different way then mailing all output to the QA mailinglist.
-             </li>
+                See <a href="http://windows.php.net/qa/">here</a> for the Windows builds.
+                <br><br>
+
+              </li>
+
+              <li>
+                Upgrading the testsuite. This includes extending the testsuites with tests for every function,
+                collecting test data in a different way then mailing all output to the QA mailinglist.
+              </li>
 
             </ul>
             <p>
@@ -48,18 +48,18 @@ common_header();
           </td>
           <td width="10">&nbsp;</td>
         </tr>
-        <tr> 
+        <tr>
           <td width="10">&nbsp;</td>
           <td width="100%">
           <br />
           <p>
-           If you would like to contribute to these efforts, please 
+           If you would like to contribute to these efforts, please
            visit our <a href="howtohelp.php">How To Help</a> page.
           </p>
 		</td>
           <td width="10">&nbsp;</td>
         </tr>
-        <tr> 
+        <tr>
           <td width="10">&nbsp;</td>
           <td width="100%">&nbsp;</td>
           <td width="10">&nbsp;</td>
