6965f2c4eac455aaa7cdd4b73957cabf18f6f987
Johannes Schlüter <johannes@php.net>
2009-05-07 16:57:07 +0000

    5.3.0RC2

--- include/release-qa.php
+++ include/release-qa.php
@@ -5,7 +5,7 @@
  *  with list of urls to the packages.
  */
 
-$BUILD_TEST_RELEASES = array('5.3.0RC1');
+$BUILD_TEST_RELEASES = array('5.3.0RC2');
 $DEV_RELEASES = array();
 
 foreach($BUILD_TEST_RELEASES as $release) {
@@ -30,7 +30,7 @@ $RC_FILES_52 = array (
 );
 
 /* PHP 5 Releases */
-$CURRENT_QA_RELEASE_5 = '5.3.0RC1'; // false
+$CURRENT_QA_RELEASE_5 = '5.3.0RC2'; // false
 $RC_FILES_5 = array (
 	array (
 		'http://downloads.php.net/johannes/',
