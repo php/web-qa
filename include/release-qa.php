a1337dc4465c01c4e598aace42bb3534db9d9d2a
Philip Olson <philip@php.net>
2009-07-15 07:01:09 +0000

    allow 6.0.0 make tests to be reported

--- include/release-qa.php
+++ include/release-qa.php
@@ -7,7 +7,7 @@
 
 // FIXME: Use http://www.php.net/releases/index.php?serialize=1 info here?
 // Note:  These two variables determine which failed make tests may report to the qa.reports list
-$BUILD_TEST_RELEASES = array('5.2.10', '5.3.0'); // array('5.3.0RC4')
+$BUILD_TEST_RELEASES = array('5.2.10', '5.3.0', '6.0.0'); // array('5.3.0RC4')
 $DEV_RELEASES = array();
 
 foreach($BUILD_TEST_RELEASES as $release) {
