cd3e261c45f9136e1b7ddcefe2d6fc1a5ac36127
Ilia Alshanetsky <iliaa@php.net>
2009-09-03 18:40:54 +0000

    5.2.11RC2

--- include/release-qa.php
+++ include/release-qa.php
@@ -7,7 +7,7 @@
 
 // FIXME: Use http://www.php.net/releases/index.php?serialize=1 info here?
 // Note:  These two variables determine which failed make tests may report to the qa.reports list
-$BUILD_TEST_RELEASES = array('5.2.11RC1');
+$BUILD_TEST_RELEASES = array('5.2.11RC2');
 $DEV_RELEASES = array();
 
 foreach($BUILD_TEST_RELEASES as $release) {
@@ -19,7 +19,7 @@ foreach($BUILD_TEST_RELEASES as $release) {
 
 $RELEASE_PROCESS = array(52 => true, 53 => false);
 
-$CURRENT_QA_RELEASE_52 = '5.2.11RC1';
+$CURRENT_QA_RELEASE_52 = '5.2.11RC2';
 $RC_FILES_52 = array (
 	array (
 		'http://downloads.php.net/ilia/',
