2f0f19f2fb9fc04c8e3d1561fb84e3738420ccae
Johannes Schlüter <johannes@schlueters.de>
2013-05-08 12:08:52 +0200

    GitHub requires a user agent nowadays

--- pulls/api.php
+++ pulls/api.php
@@ -1,6 +1,7 @@
 <?php
 $errors = array();
 require('./config.php');
+define('USER_AGENT', 'PHP Pull Request Admin (http://qa.php.net/pulls php-qa@lists.php.net)');
 
 if ($_SERVER['SERVER_NAME'] === 'schlueters.de') {
 	define('DEV', true);
@@ -178,7 +179,8 @@ function ghupdate()
 	$url = GITHUB_BASEURL.'repos/'.GITHUB_ORG.'/'.urlencode($_POST['repo']).'/pulls/'.$_POST['id'];
 	$ctxt = stream_context_create(array(
 		'http' => array(
-			'ignore_errors' => '1'
+			'ignore_errors' => '1',
+			'user_agent' => USER_AGENT,
 		)
 	));
 	$pull_raw = @file_get_contents($url, false, $ctxt);
