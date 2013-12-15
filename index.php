b5446d0cb6f3be3b0c58083c5e0a476cee55b3bf
Hannes Magnusson <bjori@mongodb.com>
2013-12-15 01:17:59 -0800

    Remove table layout

--- index.php
+++ index.php
@@ -2,35 +2,23 @@
 include("include/functions.php");
 include("include/release-qa.php");
 
-$TITLE = "PHP-QAT: Quality Assurance Team";
 $SITE_UPDATE = date("D M d H:i:s Y T", filectime(__FILE__))."<br />\n".
 '/* $Id$ */';
 
 common_header();
 
 ?>
-      <table width="70%" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-          <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
-          <td width="100%">
            <h1>Welcome to the<br />PHP Quality Assurance Team Web Page.</h1>
            <p>
             The PHP Quality Assurance Team supports the PHP Development Team by
             providing them with information on compatibility and stability issues.
            </p>
-          </td>
-          <td width="10"><img src="gfx/spacer.gif" width="10" height="1"></td>
-        </tr>
-        <tr>
-          <td width="10">&nbsp;</td>
-          <td width="100%">
-            <br />
+
             <h3>Make test results:</h3>
             <ul>
             <li>
               All users who compile PHP are encouraged to run '<a href="http://qa.php.net/running-tests.php">make test</a>', which
               runs the test suite and optionally sends the <a href=" http://news.php.net/php.qa.reports">results here</a>.
-              <br /><br />
             </li>
             <li>
               Compiled <a href="reports/">user submitted test result reports</a> for analysis
@@ -50,26 +38,10 @@ common_header();
                 See <a href="http://windows.php.net/qa/">here</a> for the Windows builds.
               </li>
             </ul>
-          </td>
-          <td width="10">&nbsp;</td>
-        </tr>
-        <tr>
-          <td width="10">&nbsp;</td>
-          <td width="100%">
-          <br />
           <p>
            If you would like to contribute to these efforts, please
            visit our <a href="howtohelp.php">How To Help</a> page.
           </p>
-		</td>
-          <td width="10">&nbsp;</td>
-        </tr>
-        <tr>
-          <td width="10">&nbsp;</td>
-          <td width="100%">&nbsp;</td>
-          <td width="10">&nbsp;</td>
-        </tr>
-      </table>
 <?php
 
 common_footer();
