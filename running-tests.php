06cb697d5ae6cf43cf4a657e35637aace9febe11
Kalle Sommer Nielsen <kalle@php.net>
2016-08-17 18:48:50 +0200

    Added a note on how to run specific tests, using make test TESTS=X and the name for make on Windows

--- running-tests.php
+++ running-tests.php
@@ -21,6 +21,19 @@ common_header();
     enables us to ask you some more information if a test failed. Note that this script 
     also uploads php -i output so your hostname may be transmitted.
    </p>
+
+   <p>
+    Specific tests can also be executed, like running tests for a certain extension. To do 
+    this you can do like so (for example the standard library): 
+    <code>make test TESTS=ext/standard</code>. Where <code>TESTS=</code> points to a 
+    directory containing <code>.phpt</code> files.
+   </p>
+
+   <p>
+    <strong>Windows users:</strong> On Windows the make command is called <code>nmake</code> 
+    instead of <code>make</code>. This means that on Windows you will have to run 
+    <code>nmake test</code>, to run the test suite.
+   </p>
 <?php
 common_footer();
 ?>
