0cbd1a67d13c5525fa5e861b50f6e0e02089f598
Kalle Sommer Nielsen <kalle@php.net>
2016-09-01 04:55:44 +0200

    Ups, didn't mean to add this character here

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ common_header();
               <li>
 <?php show_release_qa($QA_RELEASES); ?>
 
-                See <a href="http://windows.php.net/qa/">here</a> for the Windows QA builds and ¨
+                See <a href="http://windows.php.net/qa/">here</a> for the Windows QA builds and 
                 <a href="http://windows.php.net/snaps/">here</a> for the Windows Snapshot builds.
               </li>
             </ul>
