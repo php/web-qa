2f721ce62a302e7506a68191114247973ca88af6
Sara Golemon <pollita@php.net>
2018-08-24 18:00:49 -0400

    Refactor api.php

--- api.php
+++ api.php
@@ -1,58 +1,57 @@
 <?php
-require 'include/release-qa.php';
+
+// Attempt to handle an API request of either following forms:
+// api.php?type=qa-releases
+// api.php?type=qa-releases&format=json
+// api.php?type=qa-releases&format=serialize
+//
+// Missing format specifier will default to 'serialize'
+// Output HTML help if type arg is missing or invalid.
+(function() {
+	require 'include/release-qa.php';
+	!empty($QA_RELEASES) or die('Major Fail: $QA_RELEASES is empty.');
+
+	switch ($_GET['type'] ?? null) {
+		case 'qa-releases':
+			$output = $QA_RELEASES;
+			if (($_GET['only'] ?? null) === 'dev_versions') {
+				$output = $output['reported'];
+			}
+			break;
+		default:
+			return;
+	}
+
+	switch ($_GET['format'] ?? null) {
+		case 'json':
+			echo json_encode($output);
+			exit;
+		case 'serialize':
+		default:
+			echo serialize($output);
+			exit;
+	}
+})();
+
+// Fallback on presenting documentation for this script.
 require 'include/functions.php';
 
 $TITLE       = "QA API";
 $SITE_UPDATE = date("D M d H:i:s Y T", filectime(__FILE__));
-
-$types   = array('qa-releases');
-$formats = array('serialize', 'json');
-
-if (empty($QA_RELEASES)) {
-	die('Major Fail: $QA_RELEASES is empty.');
-}
-
-if (!empty($_GET['type'])) {
-	
-	if (!empty($_GET['only']) && $_GET['only'] === 'dev_versions') {
-		$output = $QA_RELEASES['reported'];
-	} else {
-		$output = $QA_RELEASES;
-	}
-	
-	$format = 'serialize';
-	if (in_array($_GET['type'], $types)) {
-		
-		if (isset($_GET['format']) && in_array($_GET['format'], $formats)) {
-			$format = $_GET['format'];
-		}
-		
-		switch ($format) {
-			case 'json':
-				echo json_encode($output);
-				break;
-			case 'serialize':
-				echo serialize($output);
-				break;
-		}
-		exit;
-	}
-}
-
 common_header();
 ?>
 <p>
 The QA API is simple, and is based on the query string. 
 Pass in type=qa-releases (the only type currently), along with the desired format (serialize or json). 
 </p>
+
 <p>
 Example URLs:
 <ul>
 <li>All information, serialized: <a href="api.php?type=qa-releases&format=serialize">http://qa.php.net/api.php?type=qa-releases&format=serialize</a></li>
 <li>Only dev version numbers, json: <a href="api.php?type=qa-releases&format=json&only=dev_versions">http://qa.php.net/api.php?type=qa-releases&format=json&only=dev_versions</a></li>
 </ul>
-
+</p>
 <?php
 common_footer();
-?>
 
