0ff9b622a13b3a4fb2553ea1215ec6480f773934
Peter Kokot <peterkokot@gmail.com>
2018-10-26 00:23:52 +0200

    Fix typo

--- pulls/api.php
+++ pulls/api.php
@@ -180,7 +180,7 @@ function ghupdate()
 
 	if (empty($_POST['id']) || !is_numeric($_POST['id'])) {
 		header('HTTP/1.0 400 Bad Request');
-		die(json_encode(['success' => false, 'errors' => ["No or inalid id provided"]]));
+		die(json_encode(['success' => false, 'errors' => ["No or invalid id provided"]]));
 	}
 
 	if (empty($_POST['comment']) || !($comment = trim($_POST['comment']))) {
