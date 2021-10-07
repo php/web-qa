2ec5fb749715b4fa12fcd6836ad007d76656eeb9
Gabriel Caruso <carusogabriel34@gmail.com>
2021-10-07 15:11:27 +0200

    Announce PHP 8.0.12RC1

--- include/release-qa.php
+++ include/release-qa.php
@@ -70,16 +70,16 @@ $QA_RELEASES = [
         ],
     ],
 
-    '8.0.11' => [
+    '8.0.12' => [
         'active'  => true,
         'release' => [
             'type'       => 'RC',
-            'number'     => 0,
-            'sha256_gz'  => '',
-            'sha256_bz2' => '',
-            'sha256_xz'  => '',
-            'date'       => '12 Aug 2021',
-            'baseurl'    => 'https://downloads.php.net/',
+            'number'     => 1,
+            'sha256_gz'  => '6cbb9cdc0371ea0883acc75efbcb579c304afacd57c197e624efc187e3f7da6a',
+            'sha256_bz2' => 'f380791dae566dde1ab8b2e52ef5435e1928dee162c1c7075be3915804cf2a33',
+            'sha256_xz'  => '29566f0731a40810ac67e813891cc2ec5d90a34a976f792e654974826ae8dc97',
+            'date'       => '07 Oct 2021',
+            'baseurl'    => 'https://downloads.php.net/~krakjoe/',
         ],
     ],
 
