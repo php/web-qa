c7b24521eb143e1101425141122d4f3153c0f6a0
Gabriel Caruso <carusogabriel34@gmail.com>
2024-01-09 01:25:35 +0100

    PHP 8.0 is EOL
    Following php/web-php#840

--- include/release-qa.php
+++ include/release-qa.php
@@ -45,19 +45,6 @@ TODO:
 
 $QA_RELEASES = [
 
-    '8.0.29' => [
-        'active'  => true,
-        'release' => [
-            'type'       => 'RC',
-            'number'     => 0,
-            'sha256_gz'  => '',
-            'sha256_bz2' => '',
-            'sha256_xz'  => '',
-            'date'       => '08 Jun 2023',
-            'baseurl'    => 'https://downloads.php.net/',
-        ],
-    ],
-
     '8.1.27' => [
         'active'  => true,
         'release' => [
