30258d82b2ee037cf614e1600c0822df655ab82b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-05-15 09:29:29 -0400

    spelling: variable
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- write-test.php
+++ write-test.php
@@ -236,7 +236,7 @@ and not in the INI section. This is because of the order in which settings are c
 date_default_timezone_set() -> TZ environmental -> INI setting -> System Setting
 </pre>
 <p>
-If a TZ environmental varaibale is found the INI setting will be ignored.
+If a TZ environmental variable is found the INI setting will be ignored.
 </p>
 
 <p>Tests that run, or only have matching EXPECT output, on 32bit
