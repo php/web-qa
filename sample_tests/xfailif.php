518ee5cf2b6ecd5d9a9f8c963f415edd462df978
Christoph M. Becker <cmbecker69@gmx.de>
2024-09-17 15:19:00 +0200

    Fix xfailif.php sample
    These samples are not meant to be executed, but rather to be shown.

--- sample_tests/xfailif.php
+++ sample_tests/xfailif.php
@@ -1,4 +1,16 @@
---TEST--
+<?php
+include("../include/functions.php");
+
+$TITLE = "Sample Test [PHP-QAT: Quality Assurance Team]";
+$SITE_UPDATE = date("D M d H:i:s Y T", filectime(__FILE__));
+
+common_header();
+?>
+
+<div style="padding: 10px">
+<h1>Sample Test: skipif2.inc</h1>
+<p>Back to &quot;<a href="../phpt_details.php">PHPT Test File Layout</a>&quot;</p>
+<pre>--TEST--
 Handling of errors during linking
 --INI--
 opcache.enable=1
@@ -6,13 +18,20 @@ opcache.enable_cli=1
 opcache.optimization_level=-1
 opcache.preload={PWD}/preload_inheritance_error_ind.inc
 --SKIPIF--
-<?php
+&lt;?php
 require_once('skipif.inc');
 if (getenv('SKIP_ASAN')) die('xfail Startup failure leak');
-?>
+?&gt;
 --FILE--
-<?php
+&lt;?php
 echo "Foobar\n";
-?>
+?&gt;
 --EXPECTF--
 Fatal error: Declaration of B::foo($bar) must be compatible with A::foo() in %spreload_inheritance_error.inc on line 8
+</pre>
+<p>Back to &quot;<a href="../phpt_details.php">PHPT Test File Layout</a>&quot;</p>
+</div>
+
+<?php
+common_footer();
+?>
